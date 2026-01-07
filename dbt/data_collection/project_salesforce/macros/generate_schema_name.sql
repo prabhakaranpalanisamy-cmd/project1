d94c5cc2127448860cad4afdd3d66064c09125ef
prabhakaranpalanisamy-cmd <prabhakaran.palanisamy@latentview.com>
2026-01-07 17:11:06 +0530

    Delete dbt directory

--- dbt/data_collection/project_salesforce/macros/generate_schema_name.sql
+++
@@ -1,8 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
-    {# If the model has a custom schema, use it alone #}
-    {% if custom_schema_name is not none %}
-        {{ custom_schema_name }}
-    {% else %}
-        {{ target.schema }}
-    {% endif %}
-{% endmacro %}
